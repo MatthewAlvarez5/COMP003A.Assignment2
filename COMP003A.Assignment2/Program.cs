097020909e28eeaf227beb1d7925b752e5df16d4
MatthewAlvarez5 <alvarezmatthew2002@gmail.com>
2023-01-25 19:21:21 -0800

    yo

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace COMP003A.Assignment2
+﻿/*
+    * Author: Matthew Alvarez
+    * Course: COMP-003A
+    * Purpose: Assignment for Week 2 
+*/
+namespace COMP003A.Assignment2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title = "COMP-003A - Assignment 2";
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            /* String section */
+            Console.WriteLine("What is Your First Name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is Your Middle Name?");
+            string middleName = Console.ReadLine();
+            Console.WriteLine("What is Your Last Name?");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("How old will you be in 2023?");
+            string inputAge = Console.ReadLine();
+            int yearBorn = 2023 - Convert.ToInt32(inputAge);
+            Console.WriteLine($"Hello, {firstName} {middleName} {lastName}. You were born in {yearBorn}.");
+
+            /* Math Section */
+            Console.WriteLine("Enter a digit:");
+            string integer1 = Console.ReadLine();
+
+            Console.WriteLine("Enter a second digit:");
+            string integer2 = Console.ReadLine();
+
         }
     }
 }
